d96d6c4df5f7e76ab4bfb7c1966e50808956ca52
Thao Bui <buithithanhthao19022005@gmail.com>
2026-07-08 16:15:04 +0000

    up

--- models/intermediate      /int_jobs_unified.sql
+++ models/intermediate      /int_jobs_unified.sql
@@ -1,15 +1,44 @@
 {{ config(materialized='view', schema='intermediate') }}
 
 with itviec as (
-    select * from {{ ref('stg_itviec__jobs') }}
+    select 
+        job_id,
+        title,
+        company,
+        location,
+        salary,
+        -- Nếu bên stg_itviec cột này đang là STRING, ta CAST nó về DATE để đồng bộ với VietnamWorks
+        cast(posted_at as DATE) as posted_at, 
+        tags,
+        url
+    from {{ ref('stg_itviec__jobs') }}
 ),
 
 vietnamworks as (
-    select * from {{ ref('stg_vietnamworks__jobs') }}
+    select 
+        job_id,
+        title,
+        company,
+        location,
+        salary,
+        posted_at, -- Cột này đã là DATE sẵn từ file Python/Staging trước đó
+        tags,
+        url
+    from {{ ref('stg_vietnamworks__jobs') }}
 ),
 
 careerviet as (
-    select * from {{ ref('stg_careerviet__jobs') }}
+    select 
+        job_id,
+        title,
+        company,
+        location,
+        salary,
+        -- Nếu bên stg_careerviet cột này đang là STRING, ta cũng CAST nó về DATE
+        cast(posted_at as DATE) as posted_at, 
+        tags,
+        url
+    from {{ ref('stg_careerviet__jobs') }}
 ),
 
 unioned as (
