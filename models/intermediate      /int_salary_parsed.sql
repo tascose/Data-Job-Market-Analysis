af3dab4be573fe3c92be44dadb0bee0a00550b36
Thao Bui <buithithanhthao19022005@gmail.com>
2026-07-08 17:16:24 +0000

    up

--- models/intermediate      /int_salary_parsed.sql
+++ models/intermediate      /int_salary_parsed.sql
@@ -1,41 +1,14 @@
 {{ config(materialized='view', schema='intermediate') }}
 
 with base as (
-    select * from {{ ref('int_role_classified') }}
+    select * from {{ ref('int_dates_normalized') }} -- Hoặc ref('int_jobs_unified')
 ),
 
 cleaned as (
     select
         *,
-        lower(trim(coalesce(salary_raw, ''))) as salary_lower
+        -- SỬA TẠI ĐÂY: Đổi salary_raw thành salary
+        trim(lower(salary)) as salary_clean 
     from base
 ),
-
-extracted as (
-    select
-        * except (salary_lower),
-        case
-            when salary_lower = '' then null
-            when regexp_contains(salary_lower, r'thương lượng|negotiate|negotiable|cạnh tranh|competitive|thoả thuận')
-                then null
-            when regexp_contains(salary_lower, r'usd|\$')
-                then cast(regexp_replace(regexp_extract(salary_lower, r'([\d,\.]+)'), r',', '') as float64) * 26000
-            when regexp_contains(salary_lower, r'triệu|tr\b|million')
-                then cast(regexp_replace(regexp_extract(salary_lower, r'([\d,\.]+)'), r',', '') as float64) * 1000000
-            else null
-        end as salary_min_vnd,
-
-        case
-            when salary_lower = '' then null
-            when regexp_contains(salary_lower, r'thương lượng|negotiate|negotiable|cạnh tranh|competitive|thoả thuận')
-                then null
-            when regexp_contains(salary_lower, r'usd|\$')
-                then (select max(cast(regexp_replace(x, r',', '') as float64)) from unnest(regexp_extract_all(salary_lower, r'[\d,]+')) x) * 26000
-            when regexp_contains(salary_lower, r'triệu|tr\b|million')
-                then (select max(cast(regexp_replace(x, r',', '') as float64)) from unnest(regexp_extract_all(salary_lower, r'[\d,]+')) x) * 1000000
-            else null
-        end as salary_max_vnd
-    from cleaned
-)
-
-select * from extracted
+...
