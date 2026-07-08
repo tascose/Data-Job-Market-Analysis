cc438f53337d05fc45330da9679118065880cba4
Thao Bui <buithithanhthao19022005@gmail.com>
2026-07-08 18:17:53 +0000

    u

--- tests/assert_no_cross_source_dupes.sql
+++ tests/assert_no_cross_source_dupes.sql
@@ -1,4 +1,13 @@
-select job_title_clean, company_name_clean, posted_date, location_clean, count(*) as cnt
+select 
+    job_title_clean, 
+    company_name_clean, 
+    posted_at, 
+    location_clean, 
+    count(*) as cnt
 from {{ ref('int_jobs_deduplicated') }}
-group by job_title_clean, company_name_clean, posted_date, location_clean
+group by 
+    job_title_clean, 
+    company_name_clean, 
+    posted_at, 
+    location_clean
 having count(*) > 1
